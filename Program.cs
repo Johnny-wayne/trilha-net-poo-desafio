8a8b1449b479cd7cabac02de01162d3b3388f3f2
Johnny-wayne <johnnywaynetattoo@gmail.com>
2024-09-09 22:40:32 -0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("IPHONE \n___________________");
+Iphone iphone = new("961232132", "11.0", "11111111", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Sudoku");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("NOKIA \n___________________");
+Nokia nokia = new("12384134", "Tijolão", "2222222", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da Velha");
